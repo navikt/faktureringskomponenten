dbd06229dab384c6632f533378e1be88cb97bc39
Isa Salem Hame <wanghattan@gmail.com>
2022-10-25 14:09:50 +0200

    Legg til databaseoppsett

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.kafka:spring-kafka-test")
 	testImplementation("org.springframework.security:spring-security-test")
+	testImplementation("com.h2database:h2")
 }
 
 tasks.withType<KotlinCompile> {
