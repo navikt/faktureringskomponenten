36287b515bd06df85a3eb503efe8b574dd466aa1
Isa Salem Hame <wanghattan@gmail.com>
2022-10-31 15:03:56 +0100

    kafka & security config

--- src/main/kotlin/no/nav/melosysfakturering/config/SecurityConfiguration.kt
+++ src/main/kotlin/no/nav/melosysfakturering/config/SecurityConfiguration.kt
@@ -1,21 +1,23 @@
 package no.nav.melosysfakturering.config
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
 
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfiguration {
 
-    @Autowired
-    @Throws(Exception::class)
-    protected fun configure(http: HttpSecurity) {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
             .authorizeRequests()
             .anyRequest()
             .permitAll()
+        return http.build();
     }
 }
