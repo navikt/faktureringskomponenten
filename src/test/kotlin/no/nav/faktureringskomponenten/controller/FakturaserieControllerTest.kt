eb3a9d9eb14545944d4193f85cb7503fb3157b20
Isa Salem Hame <wanghattan@gmail.com>
2022-11-25 14:21:07 +0100

    Gjør forbedringer av tester

--- src/test/kotlin/no/nav/faktureringskomponenten/controller/FakturaserieControllerTest.kt
+++ src/test/kotlin/no/nav/faktureringskomponenten/controller/FakturaserieControllerTest.kt
@@ -1,20 +1,26 @@
 package no.nav.faktureringskomponenten.controller
 
 import no.nav.faktureringskomponenten.controller.dto.FakturaserieDto
-import no.nav.faktureringskomponenten.controller.dto.FullmektigDto
+import no.nav.faktureringskomponenten.controller.dto.FakturaserieIntervallDto
 import no.nav.faktureringskomponenten.controller.dto.FakturaseriePeriodeDto
-import no.nav.faktureringskomponenten.controller.dto.FaktureringsIntervall
-import no.nav.faktureringskomponenten.testutils.PostgresContainer
+import no.nav.faktureringskomponenten.controller.dto.FullmektigDto
+import no.nav.faktureringskomponenten.testutils.PostgresTestContainerBase
+import org.assertj.core.internal.bytebuddy.utility.RandomString
+import org.junit.Test
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.*
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec
-import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import java.math.BigDecimal
 import java.time.LocalDate
@@ -23,14 +29,10 @@ import java.time.LocalDate
 @Testcontainers
 @AutoConfigureWebTestClient
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-internal class FakturaserieControllerTest(
+@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
+class FakturaserieControllerTest(
     @Autowired val webClient: WebTestClient
-) {
-
-    companion object {
-        @Container
-        val testContainer: PostgresContainer = PostgresContainer.setupAndStart()
-    }
+) : PostgresTestContainerBase() {
 
     private val requestHeaders: MutableMap<String, String> = mutableMapOf()
 
@@ -40,52 +42,59 @@ internal class FakturaserieControllerTest(
     }
 
     @Test
-    fun `lagNyFaktura lager ny faktura når alle verdier er riktige`() {
-        val fakturaserieDto = lagFakturaserieDto()
-
-        postLagNyFakturaRequest(fakturaserieDto)
-            .expectStatus().isOk
+    @ParameterizedTest(name = "{0} resulterer i {2}")
+    @MethodSource("fakturaserieDTOs")
+    fun `lagNyFaktura validerer input riktig`(
+        testbeskrivelse: String,
+        fakturaserieDto: FakturaserieDto,
+        forventetStatus: HttpStatus
+    ) {
+        postLagNyFakturaRequest(fakturaserieDto).expectStatus().isEqualTo(forventetStatus)
     }
 
-    @Test
-    fun `lagNyFaktura feiler når fødselsnummer ikke har riktig lengde`() {
-        val fakturaserieDto = lagFakturaserieDto(fodselsnummer = "123456")
-
-        postLagNyFakturaRequest(fakturaserieDto)
-            .expectStatus().is4xxClientError
-    }
-
-    @Test
-    fun `lagNyFaktura feiler når fødselsnummer ikke er sifre`() {
-        val fakturaserieDto = lagFakturaserieDto(fodselsnummer = "1234567891f")
-
-        postLagNyFakturaRequest(fakturaserieDto)
-            .expectStatus().is4xxClientError
-    }
-
-    @Test
-    fun `lagNyFaktura feiler når referanseNAV er blank`() {
-        val fakturaserieDto = lagFakturaserieDto(referanseNav = "")
-
-        postLagNyFakturaRequest(fakturaserieDto)
-            .expectStatus().is4xxClientError
-    }
-
-    @Test
-    fun `lagNyFaktura feiler når perioder er tom`() {
-        val fakturaserieDto = lagFakturaserieDto(fakturaseriePeriode = listOf())
-
-        postLagNyFakturaRequest(fakturaserieDto)
-            .expectStatus().is4xxClientError
+    private fun fakturaserieDTOs(): List<Arguments> {
+        return listOf(
+            arguments(
+                "DTO med riktige verdier",
+                lagFakturaserieDto(vedtaksnummer = "V1"),
+                HttpStatus.OK
+            ),
+            arguments(
+                "DTO med samme vedtaksId som tidligere",
+                lagFakturaserieDto(vedtaksnummer = "V1"),
+                HttpStatus.INTERNAL_SERVER_ERROR //TODO: Fiks errorhåndtering - https://jira.adeo.no/browse/MELOSYS-5543
+            ),
+            arguments(
+                "Fødselsnummer med feil lengde",
+                lagFakturaserieDto(fodselsnummer = "123456"),
+                HttpStatus.BAD_REQUEST
+            ),
+            arguments(
+                "Fødselsnummer med bokstaver",
+                lagFakturaserieDto(fodselsnummer = "1234567891f"),
+                HttpStatus.BAD_REQUEST
+            ),
+            arguments(
+                "referanseNAV som er blank",
+                lagFakturaserieDto(referanseNav = ""),
+                HttpStatus.BAD_REQUEST
+            ),
+            arguments(
+                "Perioder som er tom",
+                lagFakturaserieDto(fakturaseriePeriode = listOf()),
+                HttpStatus.BAD_REQUEST
+            )
+        )
     }
 
     private fun lagFakturaserieDto(
-        vedtaksnummer: String = "VEDTAK-1",
+        vedtaksnummer: String = "VEDTAK-1" + RandomString.make(3),
         fodselsnummer: String = "12345678911",
         fullmektig: FullmektigDto = FullmektigDto("11987654321", "123456789", "Ole Brum"),
         referanseBruker: String = "Nasse Nøff",
         referanseNav: String = "NAV referanse",
-        intervall: FaktureringsIntervall = FaktureringsIntervall.KVARTAL,
+        fakturaGjelder: String = "Trygdeavgift",
+        intervall: FakturaserieIntervallDto = FakturaserieIntervallDto.KVARTAL,
         fakturaseriePeriode: List<FakturaseriePeriodeDto> = listOf(
             FakturaseriePeriodeDto(
                 BigDecimal.valueOf(123),
@@ -101,6 +110,7 @@ internal class FakturaserieControllerTest(
             fullmektig,
             referanseBruker,
             referanseNav,
+            fakturaGjelder,
             intervall,
             fakturaseriePeriode
         )
---
+++ src/test/kotlin/no/nav/faktureringskomponenten/testutils/PostgresTestContainerBase.kt
@@ -0,0 +1,21 @@
+package no.nav.faktureringskomponenten.testutils
+
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+
+open class PostgresTestContainerBase {
+    companion object {
+        var dbContainer = PostgreSQLContainer("postgres:12.11")
+
+        @DynamicPropertySource
+        @JvmStatic
+        fun postgresProperties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url") { dbContainer.jdbcUrl }
+            registry.add("spring.datasource.password") { dbContainer.password }
+            registry.add("spring.datasource.username") { dbContainer.username }
+
+            dbContainer.start()
+        }
+    }
+}
