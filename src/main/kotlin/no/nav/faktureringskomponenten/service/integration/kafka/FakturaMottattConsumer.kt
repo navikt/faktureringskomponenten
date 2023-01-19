6f7223512f163487d761832895fc278c9f542d68
RuneLind <rulind@gmail.com>
2023-01-19 10:28:49 +0100

    Fjern test logging

--- src/main/kotlin/no/nav/faktureringskomponenten/service/integration/kafka/FakturaMottattConsumer.kt
+++ src/main/kotlin/no/nav/faktureringskomponenten/service/integration/kafka/FakturaMottattConsumer.kt
@@ -4,22 +4,12 @@ import no.nav.faktureringskomponenten.service.integration.kafka.dto.FakturaMotta
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Component
-class FakturaMottattConsumer(
-    @Value("\${kafka.consumer.oebs.topic}") private val topic: String,
-    @Value("\${kafka.consumer.oebs.groupid}") private val groupID: String,
-) {
+class FakturaMottattConsumer {
     private val log: Logger = LoggerFactory.getLogger(FakturaMottattConsumer::class.java)
-
-
-    init {
-        log.info("--- topic:{$topic}")
-        log.info("--- groupID:{$groupID}")
-    }
 
     @KafkaListener(
         clientIdPrefix = "melosys-faktureringskomponenten-fakturaMottatt",
