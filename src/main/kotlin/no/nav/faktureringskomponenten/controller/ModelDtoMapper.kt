f4139148e8a30d806607405eceb9b72bb6a6a03c
RuneLind <rulind@gmail.com>
2023-01-31 11:29:41 +0100

    Lag ResponseDto for hvert objekt for mer lesbar kode

--- src/main/kotlin/no/nav/faktureringskomponenten/controller/ModelDtoMapper.kt
+++ src/main/kotlin/no/nav/faktureringskomponenten/controller/ModelDtoMapper.kt
@@ -1,47 +1,56 @@
 package no.nav.faktureringskomponenten.controller
 
 import no.nav.faktureringskomponenten.controller.dto.*
+import no.nav.faktureringskomponenten.domain.models.Faktura
+import no.nav.faktureringskomponenten.domain.models.FakturaLinje
 import no.nav.faktureringskomponenten.domain.models.Fakturaserie
+import no.nav.faktureringskomponenten.domain.models.Fullmektig
 
 object ModelDtoMapper {
 
     val Fakturaserie.tilResponseDto: FakturaserieResponseDto
-        get() {
-            return FakturaserieResponseDto(
-                id = this.id,
-                vedtaksId = this.vedtaksId,
-                fakturaGjelder = this.fakturaGjelder,
-                fodselsnummer = this.fodselsnummer,
-                fullmektig = this.fullmektig.let {
-                    FullmektigDto(
-                        fodselsnummer = it?.fodselsnummer.toString(),
-                        organisasjonsnummer = it?.organisasjonsnummer,
-                        kontaktperson = it?.kontaktperson
-                    )
-                },
-                referanseBruker = this.referanseBruker,
-                referanseNAV = this.referanseNAV,
-                startdato = this.startdato,
-                sluttdato = this.sluttdato,
-                status = this.status,
-                intervall = FakturaserieIntervallDto.valueOf(this.intervall.name),
-                opprettetTidspunkt = this.opprettetTidspunkt,
-                faktura = this.faktura.map { FakturaResponseDto(
-                    id = it.id,
-                    datoBestilt = it.datoBestilt,
-                    status = it.status,
-                    fakturaLinje = it.fakturaLinje.map { fi -> FakturaLinjeResponseDto(
-                        id = fi.id,
-                        periodeFra = fi.periodeFra,
-                        periodeTil = fi.periodeTil,
-                        beskrivelse = fi.beskrivelse,
-                        belop = fi.belop,
-                        enhetsprisPerManed = fi.enhetsprisPerManed
-                    ) },
-                    fakturaserieId = it.getFakturaserieId(),
-                    periodeFra = it.getPeriodeFra(),
-                    periodeTil = it.getPeriodeTil()
-                ) }
-            )
-        }
+        get() = FakturaserieResponseDto(
+            id = this.id,
+            vedtaksId = this.vedtaksId,
+            fakturaGjelder = this.fakturaGjelder,
+            fodselsnummer = this.fodselsnummer,
+            fullmektig = this.fullmektig?.tilDto,
+            referanseBruker = this.referanseBruker,
+            referanseNAV = this.referanseNAV,
+            startdato = this.startdato,
+            sluttdato = this.sluttdato,
+            status = this.status,
+            intervall = FakturaserieIntervallDto.valueOf(this.intervall.name),
+            opprettetTidspunkt = this.opprettetTidspunkt,
+            faktura = this.faktura.map { it.tilResponseDto }
+        )
+
+    private val Fullmektig.tilDto
+        get() = FullmektigDto(
+            fodselsnummer = this.fodselsnummer.toString(),
+            organisasjonsnummer = this.organisasjonsnummer,
+            kontaktperson = this.kontaktperson
+        )
+
+    private val Faktura.tilResponseDto: FakturaResponseDto
+        get() = FakturaResponseDto(
+            id = this.id,
+            datoBestilt = this.datoBestilt,
+            status = this.status,
+            fakturaLinje = this.fakturaLinje.map { it.tilResponseDto },
+            fakturaserieId = this.getFakturaserieId(),
+            periodeFra = this.getPeriodeFra(),
+            periodeTil = this.getPeriodeTil()
+        )
+
+    private val FakturaLinje.tilResponseDto: FakturaLinjeResponseDto
+        get() = FakturaLinjeResponseDto(
+            id = this.id,
+            periodeFra = this.periodeFra,
+            periodeTil = this.periodeTil,
+            beskrivelse = this.beskrivelse,
+            belop = this.belop,
+            enhetsprisPerManed = this.enhetsprisPerManed
+        )
+
 }
