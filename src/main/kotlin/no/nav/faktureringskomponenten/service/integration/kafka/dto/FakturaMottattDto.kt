a343e6cbff1da6c3c607869f229db1f5f4fb72fa
Rune Lind <rune.lind@nav.no>
2023-02-01 14:23:02 +0100

    5707  Se at vi kan lytte på topic fra oebs (#38)

--- src/main/kotlin/no/nav/faktureringskomponenten/service/integration/kafka/dto/FakturaMottattDto.kt
+++ src/main/kotlin/no/nav/faktureringskomponenten/service/integration/kafka/dto/FakturaMottattDto.kt
@@ -1,19 +1,13 @@
 package no.nav.faktureringskomponenten.service.integration.kafka.dto
 
 import java.math.BigDecimal
-import java.time.LocalDate
 
 data class FakturaMottattDto(
 
-    val fodselsnummer: BigDecimal,
-    val fullmektigOrgnr: String?,
-    val fullmektigFnr: BigDecimal?,
-    val vedtaksId: String,
+    val fodselsnummer: String,
+    val vedtaksId: String?,
     val fakturaReferanseNr: String,
     val kreditReferanseNr: String?,
-    val referanseBruker: String,
-    val referanseNAV: String,
-    val beskrivelse: String,
-    val fakturaLinjer: List<FakturaBestiltLinjeDto>,
-    val faktureringsDato: LocalDate
+    val belop: BigDecimal,
+    val status: String,
 )
