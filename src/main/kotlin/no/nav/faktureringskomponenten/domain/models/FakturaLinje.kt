962f0e6dceb5329193d789a049174c275629f74b
RuneLind <rulind@gmail.com>
2023-02-01 12:41:23 +0100

    Legg på default verdier

--- src/main/kotlin/no/nav/faktureringskomponenten/domain/models/FakturaLinje.kt
+++ src/main/kotlin/no/nav/faktureringskomponenten/domain/models/FakturaLinje.kt
@@ -13,19 +13,19 @@ class FakturaLinje(
     val id: Long? = null,
 
     @Column(name = "periode_fra", nullable = false)
-    val periodeFra: LocalDate,
+    val periodeFra: LocalDate = LocalDate.now(),
 
     @Column(name = "periode_til", nullable = false)
-    val periodeTil: LocalDate,
+    val periodeTil: LocalDate = LocalDate.now(),
 
     @Column(name = "beskrivelse", nullable = false)
-    val beskrivelse: String,
+    val beskrivelse: String = "",
 
     @Column(name = "belop", nullable = false)
-    val belop: BigDecimal,
+    val belop: BigDecimal = BigDecimal(0),
 
     @Column(name = "enhetspris_per_maned", nullable = false)
-    val enhetsprisPerManed: BigDecimal
+    val enhetsprisPerManed: BigDecimal = BigDecimal(0)
 ) {
     @Override
     override fun toString(): String {
