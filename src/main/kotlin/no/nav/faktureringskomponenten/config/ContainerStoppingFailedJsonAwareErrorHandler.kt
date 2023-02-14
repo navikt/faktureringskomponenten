9bcf72aff6628267ef777ae799e2631832241a0d
RuneLind <rulind@gmail.com>
2023-02-14 13:06:03 +0100

    Hent ut offset når vi ikke får det fra RecordDeserializationException

--- src/main/kotlin/no/nav/faktureringskomponenten/config/ContainerStoppingFailedJsonAwareErrorHandler.kt
+++ src/main/kotlin/no/nav/faktureringskomponenten/config/ContainerStoppingFailedJsonAwareErrorHandler.kt
@@ -4,8 +4,10 @@ import no.nav.faktureringskomponenten.domain.models.FakturaMottakFeil
 import no.nav.faktureringskomponenten.domain.repositories.FakturaMottakFeilRepository
 import org.apache.kafka.clients.consumer.Consumer
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.common.errors.RecordDeserializationException
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.listener.CommonContainerStoppingErrorHandler
 import org.springframework.kafka.listener.MessageListenerContainer
 import org.springframework.stereotype.Component
@@ -13,8 +15,8 @@ import java.lang.Exception
 
 @Component
 class ContainerStoppingFailedJsonAwareErrorHandler(
-    private val valueDeserializer: DeserializerJsonAware
-
+    private val valueDeserializer: DeserializerJsonAware,
+    @Value("\${kafka.consumer.oebs.topic}") private val topic: String
 ) : CommonContainerStoppingErrorHandler() {
 
     @Autowired
@@ -26,7 +28,7 @@ class ContainerStoppingFailedJsonAwareErrorHandler(
         consumer: Consumer<*, *>,
         container: MessageListenerContainer
     ) {
-        saveError(thrownException)
+        saveError(thrownException, consumer)
         super.handleRemaining(thrownException, records, consumer, container)
     }
 
@@ -36,21 +38,25 @@ class ContainerStoppingFailedJsonAwareErrorHandler(
         container: MessageListenerContainer,
         batchListener: Boolean
     ) {
-        saveError(thrownException)
+        saveError(thrownException, consumer)
         super.handleOtherException(thrownException, consumer, container, batchListener)
     }
 
-    private fun saveError(thrownException: Exception) {
-        val failedJson = valueDeserializer.getJson()
-        if (failedJson != null) {
-            val recordDeserializationException = thrownException as? RecordDeserializationException
-            fakturaMottakFeilRepository.saveAndFlush(
-                FakturaMottakFeil(
-                    error = thrownException.cause?.message ?: thrownException.message,
-                    kafkaMelding = failedJson,
-                    kafkaOffset = recordDeserializationException?.offset()
-                )
-            )
+    private fun saveError(thrownException: Exception, consumer: Consumer<*, *>) {
+        val getOffset: () -> Long = {
+            val topicPartition = TopicPartition(topic, 0)
+            consumer.position(topicPartition)
         }
+
+        val json = valueDeserializer.getJson()
+        val recordDeserializationException = thrownException as? RecordDeserializationException
+        val offset = recordDeserializationException?.offset() ?: getOffset()
+        fakturaMottakFeilRepository.saveAndFlush(
+            FakturaMottakFeil(
+                error = thrownException.cause?.message ?: thrownException.message,
+                kafkaMelding = json,
+                kafkaOffset = offset
+            )
+        )
     }
 }
