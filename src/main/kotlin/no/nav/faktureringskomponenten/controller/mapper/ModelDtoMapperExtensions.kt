0c7bf54686cc915dde1a47ce76e9ab37ab04a7f8
Ivan Skodje <ivanskodje@protonmail.com>
2023-03-10 14:47:02 +0100

    BUGFIX: Rettet en feil med toString() som gjør at vi lagrer 'null' string i databasen, ettersom i kotlin blir null.toString() 'null'.

--- src/main/kotlin/no/nav/faktureringskomponenten/controller/mapper/ModelDtoMapperExtensions.kt
+++ src/main/kotlin/no/nav/faktureringskomponenten/controller/mapper/ModelDtoMapperExtensions.kt
@@ -53,14 +53,14 @@ fun FakturaserieIntervall.tilFakturaserieIntervallDto(): FakturaserieIntervallDt
 
 private val Fullmektig.tilDto: FullmektigDto
     get() = FullmektigDto(
-        fodselsnummer = this.fodselsnummer.toString(),
+        fodselsnummer = this.fodselsnummer,
         organisasjonsnummer = this.organisasjonsnummer,
         kontaktperson = this.kontaktperson
     )
 
 private val FullmektigDto.tilFullmektig: Fullmektig
     get() = Fullmektig(
-        fodselsnummer = this.fodselsnummer.toString(),
+        fodselsnummer = this.fodselsnummer,
         organisasjonsnummer = this.organisasjonsnummer,
         kontaktperson = this.kontaktperson
     )
