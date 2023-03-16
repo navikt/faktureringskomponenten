a7f6707ee81ebd8edfa0c19666683244180f3dc7
Ivan Skodje <ivanskodje@protonmail.com>
2023-03-16 12:15:47 +0100

    Flyttet på rekkefølge på metoder, endret litt på logging

--- src/main/kotlin/no/nav/faktureringskomponenten/service/beregning/AntallBeregner.kt
+++ src/main/kotlin/no/nav/faktureringskomponenten/service/beregning/AntallBeregner.kt
@@ -16,7 +16,7 @@ class AntallBeregner(private val fom: LocalDate, private val tom: LocalDate) {
 
     fun beregn(): BigDecimal {
         val total = beregnFørsteMånedProsent() + beregnMånederMellomProsent() + beregnSisteMånedProsent()
-        log.info("AntallBeregner fom: {}, tom: {} som gir angitt antall total: {}", fom, tom, total)
+        log.info("AntallBeregner beregner fom: { og tom: {} som gir total antall: {}", fom, tom, total)
         return total
     }
 
@@ -30,14 +30,6 @@ class AntallBeregner(private val fom: LocalDate, private val tom: LocalDate) {
         }
     }
 
-    private fun beregnSisteMånedProsent(): BigDecimal {
-        return if (erSammeMånedOgÅr) {
-            BigDecimal.ZERO
-        } else {
-            tom.dayOfMonth.toBigDecimal().divide(sisteMånedDager, 2, RoundingMode.HALF_UP)
-        }
-    }
-
     private fun beregnMånederMellomProsent(): BigDecimal {
         return if (erSammeMånedOgÅr) {
             BigDecimal.ZERO
@@ -45,6 +37,14 @@ class AntallBeregner(private val fom: LocalDate, private val tom: LocalDate) {
             val start = fom.withDayOfMonth(1).plusMonths(1)
             val end = tom.withDayOfMonth(1)
             ChronoUnit.MONTHS.between(start, end).toBigDecimal().setScale(2, RoundingMode.HALF_UP)
+        }
+    }
+
+    private fun beregnSisteMånedProsent(): BigDecimal {
+        return if (erSammeMånedOgÅr) {
+            BigDecimal.ZERO
+        } else {
+            tom.dayOfMonth.toBigDecimal().divide(sisteMånedDager, 2, RoundingMode.HALF_UP)
         }
     }
 }
