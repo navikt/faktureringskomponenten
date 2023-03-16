3069fe4932634e48b82ea80740325d0a28018686
Ivan Skodje <ivanskodje@protonmail.com>
2023-03-16 10:20:23 +0100

    Merge fix

--- src/main/resources/db/migration/V7__ny_kolonne_angitt_antall.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE faktura_linje
-    ADD COLUMN antall NUMERIC(10, 2); -- maks antall: 999,999.99
---
+++ src/main/resources/db/migration/V8__ny_kolonne_antall.sql
@@ -0,0 +1,2 @@
+ALTER TABLE faktura_linje
+    ADD COLUMN antall NUMERIC(6, 2); -- maks antall: 999.99
