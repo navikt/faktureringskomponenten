0d07e021db74b8684ebd3516ef449e49a0401c13
Francois Allix <32903762+frans-a@users.noreply.github.com>
2023-09-18 10:00:07 +0200

    Litt opprydding

--- src/main/kotlin/no/nav/faktureringskomponenten/config/ApiConfig.kt
+++ src/main/kotlin/no/nav/faktureringskomponenten/config/ApiConfig.kt
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class ApiConfig : WebMvcConfigurer{
+class ApiConfig : WebMvcConfigurer {
 
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(CorrelationIdInterceptor())
--- src/main/kotlin/no/nav/faktureringskomponenten/controller/dto/FakturaserieProblemDetailResponse.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.faktureringskomponenten.controller.dto
-
-import org.springframework.http.ProblemDetail
-
-data class ProblemDetailResponse (
-    val fakturaserieReferanse: String?
-): ProblemDetail()
