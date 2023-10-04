40d05c127b62224e5268f7965d07d9704109277d
Francois Allix <32903762+frans-a@users.noreply.github.com>
2023-10-04 17:45:25 +0200

    R

--- src/main/kotlin/no/nav/faktureringskomponenten/service/AvregningBehandler.kt
+++ src/main/kotlin/no/nav/faktureringskomponenten/service/AvregningBehandler.kt
@@ -11,28 +11,31 @@ class AvregningBehandler {
     fun lagAvregningsfaktura(fakturaseriePerioder: List<FakturaseriePeriode>, bestilteFakturaer: List<Faktura>): Faktura? {
         if (bestilteFakturaer.isEmpty()) return null
 
-        val avregningsperioder = lagEventuelleAvregningsperioder(bestilteFakturaer)
+        val avregningsperioder = lagEventuelleAvregningsperioder(bestilteFakturaer, fakturaseriePerioder)
         if (avregningsperioder.isEmpty()) return null
 
         return AvregningsfakturaGenerator().lagFaktura(avregningsperioder)
     }
 
-    private fun lagEventuelleAvregningsperioder(bestilteFakturaer: List<Faktura>): List<Avregningsperiode> {
-        return listOf(
-            Avregningsperiode(
-                periodeFra = LocalDate.of(2024, 1, 1),
-                periodeTil = LocalDate.of(2024, 3, 31),
-                bestilteFaktura = bestilteFakturaer[0],
-                tidligereBeløp = BigDecimal(9000),
-                nyttBeløp = BigDecimal(10000)
-            ),
-            Avregningsperiode(
-                periodeFra = LocalDate.of(2024, 4, 1),
-                periodeTil = LocalDate.of(2024, 6, 30),
-                bestilteFaktura = bestilteFakturaer[1],
-                tidligereBeløp = BigDecimal(9000),
-                nyttBeløp = BigDecimal(12000)
-            )
+    private fun lagEventuelleAvregningsperioder(
+        bestilteFakturaer: List<Faktura>,
+        fakturaseriePerioder: List<FakturaseriePeriode>
+    ): List<Avregningsperiode> {
+        // TODO bestilteFakturaerSomTrengerAvregning
+        return bestilteFakturaer.map { Avregningsperiode(
+            periodeFra = it.getPeriodeFra(),
+            periodeTil = it.getPeriodeTil(),
+            bestilteFaktura = it,
+            tidligereBeløp = it.totalbeløp(),
+            nyttBeløp = nyttBeløp(it.getPeriodeFra(), it.getPeriodeTil(), fakturaseriePerioder)
         )
+        }
+    }
+
+    private fun nyttBeløp(periodeFra: LocalDate, periodeTil: LocalDate, fakturaseriePerioder: List<FakturaseriePeriode>): BigDecimal {
+        if (periodeFra == LocalDate.of(2024, 1, 1)) return BigDecimal(10000)
+        if (periodeFra == LocalDate.of(2024, 4, 1)) return BigDecimal(12000)
+        return BigDecimal(1111)
+
     }
 }
