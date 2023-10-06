4f4c3b0b32c5c81881d086cdb9f676a21d1fc55d
Thang Phan <thang.quoc.ly.phan@nav.no>
2023-10-06 10:22:57 +0200

    Endrer schema desc

--- src/main/kotlin/no/nav/faktureringskomponenten/controller/dto/FakturaLinjeResponseDto.kt
+++ src/main/kotlin/no/nav/faktureringskomponenten/controller/dto/FakturaLinjeResponseDto.kt
@@ -19,9 +19,9 @@ data class FakturaLinjeResponseDto(
     @Schema(description = "Totalbeløp for hele fakturalinjen")
     val belop: BigDecimal,
 
-    @Schema(description = "Antall mnd. for fakturalinjen")
+    @Schema(description = "Antall enheter")
     val antall: BigDecimal,
 
-    @Schema(description = "Enhetspris per måned")
+    @Schema(description = "Enhetspris")
     val enhetsprisPerManed: BigDecimal
 )
