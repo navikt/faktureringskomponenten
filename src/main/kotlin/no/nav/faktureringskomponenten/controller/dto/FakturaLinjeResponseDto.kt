a00175276084afac75e06c26315323d28fa54706
Thang Phan <thang.quoc.ly.phan@nav.no>
2023-10-06 10:27:03 +0200

    6172: TOGGLE Antall linjer i respons (#90)
    * 6172: TOGGLE Antall linjer i respons
    
    * Endrer schema desc

--- src/main/kotlin/no/nav/faktureringskomponenten/controller/dto/FakturaLinjeResponseDto.kt
+++ src/main/kotlin/no/nav/faktureringskomponenten/controller/dto/FakturaLinjeResponseDto.kt
@@ -19,6 +19,9 @@ data class FakturaLinjeResponseDto(
     @Schema(description = "Totalbeløp for hele fakturalinjen")
     val belop: BigDecimal,
 
-    @Schema(description = "Enhetspris per måned")
+    @Schema(description = "Antall enheter")
+    val antall: BigDecimal,
+
+    @Schema(description = "Enhetspris")
     val enhetsprisPerManed: BigDecimal
 )
