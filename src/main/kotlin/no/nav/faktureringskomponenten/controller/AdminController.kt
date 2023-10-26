bb77b8b2ba4852e41f4d829240b21ac831026572
Thang Phan <thang.quoc.ly.phan@nav.no>
2023-10-26 13:34:52 +0200

    NO_JIRA: Tar inn endepunkter for kafka start,stop, endre offset

--- src/main/kotlin/no/nav/faktureringskomponenten/controller/AdminController.kt
+++ src/main/kotlin/no/nav/faktureringskomponenten/controller/AdminController.kt
@@ -1,14 +1,14 @@
 package no.nav.faktureringskomponenten.controller
 
-import mu.KotlinLogging
 import no.nav.faktureringskomponenten.domain.models.FakturaMottakFeil
 import no.nav.faktureringskomponenten.domain.repositories.FakturaMottakFeilRepository
+import no.nav.faktureringskomponenten.service.integration.kafka.EksternFakturaStatusConsumer
 import no.nav.security.token.support.core.api.Protected
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
-
-private val log = KotlinLogging.logger { }
 
 @Protected
 @Validated
@@ -16,6 +16,7 @@ private val log = KotlinLogging.logger { }
 @RequestMapping("/admin")
 class AdminController(
     val fakturaMottakFeilRepository: FakturaMottakFeilRepository,
+    val eksternFakturaStatusConsumer: EksternFakturaStatusConsumer
 ) {
     @GetMapping("/faktura/mottak/feil")
     fun hentFakturaMottakFeil(): ResponseEntity<Map<Long?, List<FakturaMottakFeil>>> {
@@ -24,4 +25,28 @@ class AdminController(
         return ResponseEntity.ok(groupBy.toSortedMap(compareBy { it }))
     }
 
+    @PostMapping("/faktura/mottak/consumer/stop")
+    fun stoppKafkaConsumer(): ResponseEntity<String> {
+        log.info("Stopper faktura mottat consumer")
+        eksternFakturaStatusConsumer.stop()
+        return ResponseEntity.ok("Stoppet faktura mottat consumer")
+    }
+
+    @PostMapping("/faktura/mottak/consumer/start")
+    fun startKafkaConsumer(): ResponseEntity<String> {
+        log.info("Starter faktura mottak consumer")
+        eksternFakturaStatusConsumer.start()
+        return ResponseEntity.ok("Startet faktura mottak consumer")
+    }
+
+    @PostMapping("/faktura/mottak/consumer/seek/{offset}")
+    fun settKafkaOffset(@PathVariable offset: Long): ResponseEntity<String> {
+        log.info("setter offset for faktura mottak consumer til: $offset")
+        eksternFakturaStatusConsumer.settSpesifiktOffsetPåConsumer(offset)
+        return ResponseEntity.ok("satt offset for faktura mottak consumer")
+    }
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(AdminController::class.java)
+    }
 }
