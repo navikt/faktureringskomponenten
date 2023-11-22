495b234a163a49457ba299656e03d63035862797
Eirik Bergande <eirik.bergande@nav.no>
2023-11-22 12:39:32 +0100

    Beskrivelse på faktura dto skal ikke bruke dagens dato men perioden

--- src/test/kotlin/no/nav/faktureringskomponenten/service/mappers/FakturaBestiltDtoMapperTest.kt
+++ src/test/kotlin/no/nav/faktureringskomponenten/service/mappers/FakturaBestiltDtoMapperTest.kt
@@ -70,6 +70,19 @@ class FakturaBestiltDtoMapperTest {
         fakturaBestiltDto.fakturaLinjer[0].beskrivelse shouldBe linje.beskrivelse
     }
 
+    @Test
+    fun `faktura har rett beskrivelse for kvartal`() {
+        val linje = lagFakturaLinje(false)
+        val fakturaBestiltDto = FakturaBestiltDtoMapper().tilFakturaBestiltDto(
+            Faktura(
+                fakturaLinje = listOf(linje)
+            ),
+            Fakturaserie(fakturaGjelderInnbetalingstype = Innbetalingstype.TRYGDEAVGIFT, intervall = FakturaserieIntervall.KVARTAL)
+        )
+
+        fakturaBestiltDto.beskrivelse shouldBe "Faktura Trygdeavgift 1. kvartal 2024"
+    }
+
     private fun lagFakturaLinje(erAvregning: Boolean): FakturaLinje = FakturaLinje(
         referertFakturaVedAvregning = if (erAvregning) Faktura() else null,
         periodeFra = LocalDate.of(2024, 1, 1),
