473dd488951d0f715993f32d1126a6c4e38e93e1
Isa Salem Hame <isa.salem.hame@nav.no>
2024-01-08 13:04:57 +0100

    NOJIRA / 6291: Lag endepunkt for å bestille feilet faktura (#153)
    * NOJIRA / 6291: Endepunkt for å kunne bestille feilet faktura
    
    * resend -> rebestill
    
    * Redundant semicolon
    
    * Disable test
    
    * Fiks tekst som kommer i responsen
    
    * Tom linje

---
+++ src/main/kotlin/no/nav/faktureringskomponenten/service/FakturaService.kt
@@ -0,0 +1,12 @@
+package no.nav.faktureringskomponenten.service
+
+import no.nav.faktureringskomponenten.domain.repositories.FakturaRepository
+import org.springframework.stereotype.Component
+
+@Component
+class FakturaService(
+    private val fakturaRepository: FakturaRepository,
+) {
+
+    fun hentFaktura(fakturaReferanseNr: String) = fakturaRepository.findByReferanseNr(fakturaReferanseNr)
+}
