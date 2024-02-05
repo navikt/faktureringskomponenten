131ecf27594984fe08f706a1bc2c625d38aaeadd
Isa Salem Hame <wanghattan@gmail.com>
2024-02-05 13:08:30 +0100

    Index på dato_bestilt + status

--- src/main/resources/db/migration/V28__opprett_indexer.sql
+++ src/main/resources/db/migration/V28__opprett_indexer.sql
@@ -8,3 +8,5 @@ CREATE INDEX faktura_status_idx ON faktura (status);
 CREATE INDEX faktura_linje_faktura_id_idx ON faktura_linje (faktura_id);
 
 CREATE INDEX avregning_faktura_id_idx ON faktura (avregning_faktura_id);
+
+CREATE INDEX faktura_dato_bestilt_status_idx ON faktura (dato_bestilt, status);
