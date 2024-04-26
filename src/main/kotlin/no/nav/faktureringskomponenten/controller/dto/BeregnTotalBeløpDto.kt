d9a5debd923b2e22f14042892aa5cb58bbfad41d
barstadEkstern <oystein.madsen.barstad@nav.no>
2024-04-26 14:08:53 +0200

    beregning av totalBeløp

--- src/main/kotlin/no/nav/faktureringskomponenten/controller/dto/BeregnTotalBeløpDto.kt
+++ src/main/kotlin/no/nav/faktureringskomponenten/controller/dto/BeregnTotalBeløpDto.kt
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 import no.nav.faktureringskomponenten.domain.models.FakturaseriePeriode
 import java.time.LocalDate
 
-@Schema(description = "Model for en linje i fakturaen. Liste av linjer gir grunnlag for hele fakturabeløpet")
+@Schema(description = "Model for å regne ut totalbeløp")
 data class BeregnTotalBeløpDto(
 
     @Schema(description = "Startdato for perioden")
