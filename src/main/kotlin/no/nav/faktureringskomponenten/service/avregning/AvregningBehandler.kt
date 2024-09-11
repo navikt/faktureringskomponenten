04dd5a1c8b99bbd7122ae57f13d376a22099f089
Isa Salem Hame <isa.salem.hame@nav.no>
2024-09-11 09:32:54 +0200

    ta med nullbeløp likevel

--- src/main/kotlin/no/nav/faktureringskomponenten/service/avregning/AvregningBehandler.kt
+++ src/main/kotlin/no/nav/faktureringskomponenten/service/avregning/AvregningBehandler.kt
@@ -39,7 +39,7 @@ class AvregningBehandler(private val avregningsfakturaGenerator: Avregningsfaktu
         log.debug { "Avregningsperioder generert: $avregningsperioder" }
 
         return avregningsperioder.filterNot {
-            (it.nyttBeløp > BigDecimal.ZERO && it.tidligereBeløp < BigDecimal.ZERO && it.bestilteFaktura.erAvregningsfaktura()) || it.nyttBeløp === BigDecimal.ZERO
+            it.nyttBeløp > BigDecimal.ZERO && it.tidligereBeløp < BigDecimal.ZERO && it.bestilteFaktura.erAvregningsfaktura()
         }.map {
             avregningsfakturaGenerator.lagFaktura(it)
         }.toList()
