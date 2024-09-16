017b7931816ba7a7bb8fb0963de1a2c3b0c5e69f
Isa Salem Hame <isa.salem.hame@nav.no>
2024-09-16 15:03:33 +0200

    MELOSYS-6801: Fiks feil med lagring av faktura, fiks beskrivelse

--- src/main/kotlin/no/nav/faktureringskomponenten/service/avregning/AvregningsfakturaGenerator.kt
+++ src/main/kotlin/no/nav/faktureringskomponenten/service/avregning/AvregningsfakturaGenerator.kt
@@ -8,6 +8,7 @@ import ulid.ULID
 import java.math.BigDecimal
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
 
@@ -22,15 +23,12 @@ class AvregningsfakturaGenerator {
                 id = null,
                 periodeFra = avregningsperiode.periodeFra,
                 periodeTil = avregningsperiode.periodeTil,
-                beskrivelse = "Periode: ${avregningsperiode.periodeFra.format(dateFormat)} - ${
-                    avregningsperiode.periodeTil.format(
-                        dateFormat
-                    )
-                }\nNytt beløp: ${
-                    decimalFormat.format(
-                        avregningsperiode.nyttBeløp
-                    )
-                } - tidligere beløp: ${decimalFormat.format(avregningsperiode.tidligereBeløp)}",
+                beskrivelse = genererBeskrivelse(
+                    avregningsperiode.periodeFra,
+                    avregningsperiode.periodeTil,
+                    avregningsperiode.nyttBeløp,
+                    avregningsperiode.tidligereBeløp
+                ),
                 enhetsprisPerManed = (avregningsperiode.nyttBeløp - avregningsperiode.tidligereBeløp).abs(),
                 antall = if (avregningsperiode.nyttBeløp - avregningsperiode.tidligereBeløp < BigDecimal.ZERO) BigDecimal(
                     -1
@@ -40,8 +38,6 @@ class AvregningsfakturaGenerator {
                 belop = avregningsperiode.nyttBeløp - avregningsperiode.tidligereBeløp,
             )
 
-        // status settes til BESTILT hvis det ikke er endring i hva som skal betales for gjeldende faktura og dermed ikke
-        // skal sendes til OEBS. Grunnen til at vi oppretter en faktura er for å koble tidligere faktura med en ny avregning
         return Faktura(
             referanseNr = ULID.randomULID(),
             krediteringFakturaRef = avregningsperiode.opprinneligFaktura.referanseNr,
@@ -49,5 +45,22 @@ class AvregningsfakturaGenerator {
             status = if (avregningsperiode.nyttBeløp.compareTo(avregningsperiode.tidligereBeløp) != 0) FakturaStatus.OPPRETTET else FakturaStatus.BESTILT,
             referertFakturaVedAvregning = avregningsperiode.bestilteFaktura
         )
+    }
+
+    companion object {
+        private val decimalFormat = DecimalFormat("0.00", DecimalFormatSymbols(Locale("no", "NO", "nb")))
+        private val dateFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+
+        @JvmStatic
+        fun genererBeskrivelse(
+            periodeFra: LocalDate,
+            periodeTil: LocalDate,
+            nyttBeløp: BigDecimal,
+            tidligereBeløp: BigDecimal
+        ): String {
+            return "Periode: ${periodeFra.format(dateFormat)} - ${periodeTil.format(dateFormat)}\n" +
+                    "Nytt beløp: ${decimalFormat.format(nyttBeløp)} - " +
+                    "tidligere beløp: ${decimalFormat.format(tidligereBeløp)}"
+        }
     }
 }
