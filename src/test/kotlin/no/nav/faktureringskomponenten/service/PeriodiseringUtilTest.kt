7c58d68effef4e5f3a924dea42af6f2994398555
Francois Allix <francois.allix@nav.no>
2024-11-27 15:46:02 +0100

    Lesbarhet

--- src/test/kotlin/no/nav/faktureringskomponenten/service/PeriodiseringUtilTest.kt
+++ src/test/kotlin/no/nav/faktureringskomponenten/service/PeriodiseringUtilTest.kt
@@ -11,20 +11,40 @@ class PeriodiseringUtilTest {
 
     @Test
     fun `substract a period`() {
-        val substracted = LocalDateRange.of(LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 10)).substract(
-            LocalDateRange.of(LocalDate.of(2024, 1, 3), LocalDate.of(2024, 2, 12))
+        val substracted = LocalDateRange.of(
+            LocalDate.of(2024, 1, 1),
+            LocalDate.of(2024, 1, 10)
+        ).substract(
+            LocalDateRange.of(
+                LocalDate.of(2024, 1, 3),
+                LocalDate.of(2024, 2, 12)
+            )
         )
 
-        substracted shouldBe listOf(LocalDateRange.of(LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 2)))
+        substracted shouldBe listOf(
+            LocalDateRange.of(
+                LocalDate.of(2024, 1, 1),
+                LocalDate.of(2024, 1, 2)
+            )
+        )
     }
+
 
     @Test
     fun `substract a period in the middle`() {
-        val substracted = LocalDateRange.of(LocalDate.of(2023, 12, 16), LocalDate.of(2024, 1, 15)).substract(
-            LocalDateRange.of(LocalDate.of(2023, 12, 31), LocalDate.of(2024, 1, 13))
+        val substracted = LocalDateRange.of(
+            LocalDate.of(2023, 12, 16),
+            LocalDate.of(2024, 1, 15)
+        ).substract(
+            LocalDateRange.of(
+                LocalDate.of(2023, 12, 31),
+                LocalDate.of(2024, 1, 13)
+            )
         )
 
-        substracted.shouldContainExactlyInAnyOrder(LocalDateRange.of(LocalDate.of(2023, 12, 16), LocalDate.of(2023, 12, 30)),
-            LocalDateRange.of(LocalDate.of(2024, 1, 14), LocalDate.of(2024, 1, 15)))
+        substracted.shouldContainExactlyInAnyOrder(
+            LocalDateRange.of(LocalDate.of(2023, 12, 16), LocalDate.of(2023, 12, 30)),
+            LocalDateRange.of(LocalDate.of(2024, 1, 14), LocalDate.of(2024, 1, 15))
+        )
     }
 }
