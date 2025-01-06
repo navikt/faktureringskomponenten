3fe5b15304b370fd9d850dc0cacf4c7e43d6c1c9
Francois <francois.allix@nav.no>
2025-01-06 10:08:27 +0100

    6570 Fjern ubrukt kode og oppdater ADR (#210)
    Fjern ubrukt kode og oppdater ADR

--- src/test/kotlin/no/nav/faktureringskomponenten/service/beregning/BeløpBeregnerTest.kt
+++ src/test/kotlin/no/nav/faktureringskomponenten/service/beregning/BeløpBeregnerTest.kt
@@ -2,7 +2,6 @@ package no.nav.faktureringskomponenten.service.beregning
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
-import no.nav.faktureringskomponenten.domain.models.FakturaseriePeriode
 import org.junit.jupiter.api.Test
 import java.math.BigDecimal
 import java.time.LocalDate
@@ -151,34 +150,5 @@ class BeløpBeregnerTest {
         shouldThrow<ArithmeticException> {
             BeløpBeregner.beløpForPeriode(BigDecimal("1002.25"), fom, tom)
         }
-    }
-
-    @Test
-    fun `Totalbeløp, En periode for hele måneder`() {
-        val fom = LocalDate.of(2023, 1, 1)
-        val tom = LocalDate.of(2023, 5, 31)
-        val fakturaseriePeriode = FakturaseriePeriode(BigDecimal.valueOf(700), fom, tom, "dummy")
-        val fakturaseriePerioder = listOf(fakturaseriePeriode)
-
-        val result = BeløpBeregner.totalBeløpForAllePerioder(fakturaseriePerioder)
-
-        val forventetBeløp = BigDecimal("3500.00")
-        result.shouldBe(forventetBeløp)
-    }
-
-    @Test
-    fun `Totalbeløp, Ulike perioder med ulik trygdeavgift`() {
-        val fom = LocalDate.of(2023, 1, 13)
-        val tom = LocalDate.of(2023, 12, 31)
-        val fakturaseriePeriode = FakturaseriePeriode(BigDecimal.valueOf(500), fom, tom, "dummy")
-        val fom2 = LocalDate.of(2023, 6, 1)
-        val tom2 = LocalDate.of(2023, 12, 15)
-        val fakturaseriePeriode2 = FakturaseriePeriode(BigDecimal.valueOf(1000), fom2, tom2, "dummy")
-        val fakturaseriePerioder = listOf(fakturaseriePeriode, fakturaseriePeriode2)
-
-        val result = BeløpBeregner.totalBeløpForAllePerioder(fakturaseriePerioder)
-
-        val forventetBeløp = BigDecimal("12285.00")
-        result.shouldBe(forventetBeløp)
     }
 }
